aa5637e251cea7fc9e935c5fa8cbe6ae93e2e674
arpitarikibe23 <arpitarikibe23@gmail.com>
2025-02-11 09:18:38 +0530

    Update odd_even.py

--- odd_even.py
+++ odd_even.py
@@ -1,9 +1,9 @@
 
-num = int(input("Enter a number: "))
-if num % 2 == 0:
-    print(f"{num} is even")
+no = int(input("Enter a number: "))
+if no % 2 == 0:
+    print(f"{no} is even")
 else:
-    print(f"{num} is odd")
+    print(f"{no} is odd")
 
 
 
